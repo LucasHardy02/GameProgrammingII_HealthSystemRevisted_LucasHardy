9369721763c56b0ccc8ab092f41d65c7b06313a2
Lucas Hardy <lucashardywork@gmail.com>
2026-01-09 12:18:22 -0400

    Initial commit

---
+++ GameProgrammingII_HealthSystemRevisted_LucasHardy/Program.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameProgrammingII_HealthSystemRevisted_LucasHardy
+{
+    internal class Player
+    {
+        private string _name;
+        public string Name
+        {
+            get { return _name; } 
+            private set { _name = value; }
+        }
+
+        public Health Health;
+        public Health Shield;
+
+        public void TakeDamage(int damageAmount)
+        {
+            if (Health.Shield > 0)
+            {
+
+            }
+        }
+    }
+    internal class Health
+    {
+        public Health(int maxHealth) 
+        { 
+            _maxHealth = maxHealth;
+        }
+        private float _currentHealth;
+
+        public float CurrentHealth
+        {
+            get { return _currentHealth; }
+            private set { _currentHealth = value; }
+        }
+
+        private float _maxHealth;
+
+        public float MaxHealth
+        {
+            get { return MaxHealth; }
+            private set { MaxHealth = value; }
+        }
+
+        public void TakeDamage(int damage)
+        {
+            if (damage < 0)
+            {
+                damage = 0;
+                Console.WriteLine("Attemted to recieve damage with a negative value");
+            }
+            if (damage > CurrentHealth)
+            {
+                CurrentHealth = 0;
+                Console.WriteLine("Damage was greater than players health");
+                HealthStatus();
+            }
+            else
+            {
+                CurrentHealth -= damage;
+            }
+        }
+
+        public void Restore()
+        {
+            CurrentHealth = MaxHealth;
+        }
+
+        public void Heal(int healingAmount)
+        {
+            if (healingAmount < 0)
+            {
+                healingAmount = 0;
+                Console.WriteLine("Attemted to heal with a negative value");
+            }
+
+            if (CurrentHealth == MaxHealth)
+            {
+                CurrentHealth = MaxHealth;
+                Console.WriteLine("Attemted to heal past full health")
+                HealthStatus();
+            }
+            else
+            {
+                CurrentHealth += healingAmount;
+            }
+        }
+        public void HealthStatus()
+        {
+            if (CurrentHealth == 100)
+            {
+                Console.WriteLine("Character is Healthy");
+            }
+            if (CurrentHealth < 100)
+            {
+                Console.WriteLine("Character suffered minor damage");
+            }
+            if (CurrentHealth < 75)
+            {
+                Console.WriteLine("Charcter suffered moderate damage");
+            }
+            if (CurrentHealth < 50)
+            {
+                Console.WriteLine("Character suffered major damage");
+            }
+            if (CurrentHealth < 25)
+            {
+                Console.WriteLine("Character is on their last legs");
+            }
+            if (CurrentHealth == 0)
+            {
+                Console.WriteLine("Character is dead");
+            }
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
